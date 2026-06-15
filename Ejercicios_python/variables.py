8545c0c912c484e34c8f1fe04ee0ff2f969bff5b
Irene <irenemonteroarribas@gmail.com>
2026-06-15 11:11:52 +0200

    primera variable python

---
+++ Ejercicios_python/variables.py
@@ -0,0 +1,7 @@
+#import this
+#print("Hola mundo")
+nombre = "Irene"
+edad = 38
+me_gusta_la_pizza = True
+print(f"Hola, soy {nombre}, tengo {edad} años")
+print (f"¿Me gusta la pizza? {me_gusta_la_pizza}")
